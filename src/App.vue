6422268c9642eb7fe5c4d06e5a5c7343669f8aab
Johnny Ruiz <jeko2000@yandex.com>
2021-09-17 18:59:53 -0400

    Add basic App component

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<h2>{{ name }}</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      name: 'Hello world'
+    }
+  }
+});
+</script>
